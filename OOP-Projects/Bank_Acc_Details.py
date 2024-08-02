e4e8a32c4d0fbf675b6f5ade6e2c61724b65a159
Mahan <mahankaliyamurthy@gmail.com>
2024-08-02 22:50:29 +0530

    Update Bank_Acc_Details.py

--- OOP-Projects/Bank_Acc_Details.py
+++ OOP-Projects/Bank_Acc_Details.py
@@ -29,10 +29,10 @@ class BankAccount:
     
 acc1 = BankAccount("Mahan",10000)
 acc1.deposit(20000)
-print(f'Mahan"s {acc1.get_balance()}')
+print(f'Mahan"s {acc1.get_balance()}')                                                     # Output = Mahan"s Current Balance is 30000
 acc1.withdraw(2000)
-print(f'{acc1.name} account Number is {acc1.defaultAccNumber} and {acc1.get_balance()}')
+print(f'{acc1.name} account Number is {acc1.defaultAccNumber} and {acc1.get_balance()}')   # Output = Mahan account Number is 2 and Current Balance is 28000
 
-acc2 = BankAccount("Umar",10000)
+acc2 = BankAccount("Gopal",10000)
 acc2.deposit(10000)
-print(f'{acc2.name} account number is {acc2.defaultAccNumber} and {acc2.get_balance()}')
+print(f'{acc2.name} account number is {acc2.defaultAccNumber} and {acc2.get_balance()}')   # Output = Gopal account number is 3 and Current Balance is 20000
